fb0ac82ca74e8ace0e09108c1e0d873cc0c29f3d
Polarix <326684221@qq.com>
2021-07-06 21:58:54 +0800

    2021-07-06:
    修正在某些版本的GCC/MinGW下编译错误的问题。
    删除未完成的接口。

--- GUI/inc/SGUI_PolarCoordinates.h
+++ GUI/inc/SGUI_PolarCoordinates.h
@@ -15,6 +15,5 @@
 //=======================================================================//
 void SGUI_PolarCoord_GetPoint(SGUI_INT iPoleX, SGUI_INT iPoleY, SGUI_INT IAngle, SGUI_INT iModulus, SGUI_INT* piPx, SGUI_INT* piPy);
 void SGUI_PolarCoord_DrawLine(SGUI_SCR_DEV* pstDeviceIF, SGUI_INT iPoleX, SGUI_INT iPoleY, SGUI_INT iAngle, SGUI_INT iStart, SGUI_INT iEnd, SGUI_COLOR eColor);
-void SGUI_PolarCoord_DrawArc(SGUI_SCR_DEV* pstDeviceIF, SGUI_INT iCx, SGUI_INT iCy, SGUI_INT iRadius, SGUI_INT iStartAngle, SGUI_INT iEndAngle, SGUI_COLOR eColor);
 
 #endif // _INCLUDE_SGUI_POLAR_COORDINATES_H_
